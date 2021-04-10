dcd52a3468d10cf78bcee7a8aa9fc549613f6670
Led <ledest@gmail.com>
2021-04-11 02:05:38 +0300

    update tests

--- src/ecbor.erl
+++ src/ecbor.erl
@@ -319,52 +319,8 @@ dec_array(B) ->
 -include_lib("eunit/include/eunit.hrl").
 -include_lib("otpbp/include/otpbp_pt.hrl").
 
-encode_test() ->
-    lists:foreach(fun({V, E}) -> ?_assertEqual(encode(V), binary:decode_hex(E)) end,
-                  [{0, <<"00">>},
-                   {1, <<"01">>}, {23, <<"17">>}, {24, <<"1818">>}, {16#FF, <<"18FF">>},
-                   {16#100, <<"190100">>}, {16#FFFF, <<"19FFFF">>},
-                   {16#10000, <<"1A00010000">>}, {16#FFFFFFFF, <<"1AFFFFFFFF">>},
-                   {16#100000000, <<"1B0000000100000000">>}, {16#FFFFFFFFFFFFFFFF, <<"1BFFFFFFFFFFFFFFFF">>},
-                   {-1, <<"20">>}, {-24, <<"37">>}, {-25, <<"3818">>}, {-16#100, <<"38FF">>},
-                   {-16#101, <<"390100">>}, {-16#10000, <<"39FFFF">>},
-                   {-16#10001, <<"3A00010000">>}, {-16#100000000, <<"3AFFFFFFFF">>},
-                   {-16#100000001, <<"3B0000000100000000">>}, {-16#10000000000000000, <<"3BFFFFFFFFFFFFFFFF">>},
-                   {<<"">>, <<"40">>}, {<<1, 2, 3, 4>>, <<"4401020304">>},
-                   {<<"a">>, <<"4161">>}, {<<"IETF">>, <<"4449455446">>}, {<<"\"\\">>, <<"42225c">>},
-                   {{1, 2, 3, 4}, <<"8401020304">>},
-                   {list_to_tuple(lists:seq(1, 25)), <<"98190102030405060708090a0b0c0d0e0f101112131415161718181819">>},
-                   {false, <<"F4">>}, {true, <<"F5">>}, {null, <<"F6">>}, {undefined, <<"F7">>}]).
-
-decode_test() ->
-    lists:foreach(fun({V, E}) -> ?_assertEqual(V, decode(binary:decode_hex(E))) end,
-                  [{0, <<"00">>},
-                   {1, <<"01">>}, {23, <<"17">>}, {24, <<"1818">>}, {16#FF, <<"18FF">>},
-                   {16#100, <<"190100">>}, {16#FFFF, <<"19FFFF">>},
-                   {16#10000, <<"1A00010000">>}, {16#FFFFFFFF, <<"1AFFFFFFFF">>},
-                   {16#100000000, <<"1B0000000100000000">>}, {16#FFFFFFFFFFFFFFFF, <<"1BFFFFFFFFFFFFFFFF">>},
-                   {-1, <<"20">>}, {-24, <<"37">>}, {-25, <<"3818">>}, {-16#100, <<"38FF">>},
-                   {-16#101, <<"390100">>}, {-16#10000, <<"39FFFF">>},
-                   {-16#10001, <<"3A00010000">>}, {-16#100000000, <<"3AFFFFFFFF">>},
-                   {-16#100000001, <<"3B0000000100000000">>}, {-16#10000000000000000, <<"3BFFFFFFFFFFFFFFFF">>},
-                   {positive_infinity, <<"F97C00">>}, {nan, <<"F97E00">>}, {negative_infinity, <<"F9FC00">>},
-                   {positive_infinity, <<"FA7F800000">>}, {nan, <<"FA7FC00000">>}, {negative_infinity, <<"FAFF800000">>},
-                   {positive_infinity, <<"FB7FF0000000000000">>},
-                   {nan, <<"FB7FF8000000000000">>},
-                   {negative_infinity, <<"FBFFF0000000000000">>},
-                   {<<"">>, <<"40">>}, {<<1, 2, 3, 4>>, <<"4401020304">>},
-                   {<<"">>, <<"60">>}, {<<"a">>, <<"6161">>}, {<<"IETF">>, <<"6449455446">>}, {<<"\"\\">>, <<"62225c">>},
-                   {<<"2013-03-21T20:04:00Z">>, <<"C074323031332D30332D32315432303A30343A30305A">>},
-                   {1363896240, <<"C11A514B67B0">>},
-                   {1363896240.5, <<"C1FB41D452D9EC200000">>},
-                   {<<1, 2, 3, 4>>, <<"D74401020304">>},
-                   {<<"dIETF">>, <<"D818456449455446">>},
-                   {<<"http://www.example.com">>, <<"D82076687474703A2F2F7777772E6578616D706C652E636F6D">>},
-                   {{1, 2, 3, 4}, <<"8401020304">>},
-                   {list_to_tuple(lists:seq(1, 25)), <<"98190102030405060708090a0b0c0d0e0f101112131415161718181819">>},
-                   {false, <<"F4">>}, {true, <<"F5">>}, {null, <<"F6">>}, {undefined, <<"F7">>}]).
-
 bench_test() ->
+    erlang:garbage_collect(),
     N = 10000,
     lists:foreach(fun({Name, Hex}) ->
                       Bin = binary:decode_hex(Hex),
@@ -379,4 +335,79 @@ bench_test() ->
 
 repeat_decode_n(0, _) -> ok;
 repeat_decode_n(N, Bin) -> decode(Bin), repeat_decode_n(N - 1, Bin).
+
+encode_test_() ->
+    lists:map(fun({V, E}) -> {timeout, 30, ?_assertEqual(encode(V), binary:decode_hex(E))};
+                 ({T, V, E}) -> {T, timeout, 30, ?_assertEqual(encode(V), binary:decode_hex(E))}
+              end,
+              [{0, <<"00">>},
+               {1, <<"01">>}, {23, <<"17">>}, {24, <<"1818">>}, {16#FF, <<"18FF">>},
+               {16#100, <<"190100">>}, {16#FFFF, <<"19FFFF">>},
+               {16#10000, <<"1A00010000">>}, {16#FFFFFFFF, <<"1AFFFFFFFF">>},
+               {16#100000000, <<"1B0000000100000000">>}, {16#FFFFFFFFFFFFFFFF, <<"1BFFFFFFFFFFFFFFFF">>},
+               {-1, <<"20">>}, {-24, <<"37">>}, {-25, <<"3818">>}, {-16#100, <<"38FF">>},
+               {-16#101, <<"390100">>}, {-16#10000, <<"39FFFF">>},
+               {-16#10001, <<"3A00010000">>}, {-16#100000000, <<"3AFFFFFFFF">>},
+               {-16#100000001, <<"3B0000000100000000">>}, {-16#10000000000000000, <<"3BFFFFFFFFFFFFFFFF">>},
+               {"Binary", <<"">>, <<"40">>},
+               {"Binary", <<1, 2, 3, 4>>, <<"4401020304">>},
+               {"Binary", <<"a">>, <<"4161">>},
+               {"Binary", <<"IETF">>, <<"4449455446">>},
+               {"Binary", <<"\"\\">>, <<"42225c">>},
+               {"Tiny Tuple", {1, 2, 3, 4}, <<"8401020304">>},
+               {"Small Tuple", list_to_tuple(lists:seq(1, 25)), <<"98190102030405060708090a0b0c0d0e0f101112131415161718181819">>},
+               {"Small Tuple", erlang:make_tuple(16#FF, 0), <<"98FF", (binary:copy(<<"00">>, 16#FF))/binary>>},
+               {"Short Tuple", erlang:make_tuple(16#100, 1), <<"990100", (binary:copy(<<"01">>, 16#100))/binary>>},
+               {"Short Tuple", erlang:make_tuple(16#FFFF, 1), <<"99FFFF", (binary:copy(<<"01">>, 16#FFFF))/binary>>},
+               {"Long Tuple", erlang:make_tuple(16#10000, 2), <<"9A00010000", (binary:copy(<<"02">>, 16#10000))/binary>>},
+               {"Long Tuple", erlang:make_tuple(16#FFFFFF, 2), <<"9A00FFFFFF", (binary:copy(<<"02">>, 16#FFFFFF))/binary>>},
+               {"List", [1, 2, 3, 4], <<"9F01020304FF">>},
+               {"Boolean", false, <<"F4">>}, {"Boolean", true, <<"F5">>},
+               {"NULL", null, <<"F6">>}, {"Undefined", undefined, <<"F7">>}]).
+
+decode_test_() ->
+    lists:map(fun({V, E}) -> {timeout, 30, ?_assertEqual(V, decode(binary:decode_hex(E)))};
+                 ({T, V, E}) -> {T, timeout, 30, ?_assertEqual(V, decode(binary:decode_hex(E)))}
+              end,
+              [{0, <<"00">>},
+               {1, <<"01">>}, {23, <<"17">>}, {24, <<"1818">>}, {16#FF, <<"18FF">>},
+               {16#100, <<"190100">>}, {16#FFFF, <<"19FFFF">>},
+               {16#10000, <<"1A00010000">>}, {16#FFFFFFFF, <<"1AFFFFFFFF">>},
+               {16#100000000, <<"1B0000000100000000">>}, {16#FFFFFFFFFFFFFFFF, <<"1BFFFFFFFFFFFFFFFF">>},
+               {-1, <<"20">>}, {-24, <<"37">>}, {-25, <<"3818">>}, {-16#100, <<"38FF">>},
+               {-16#101, <<"390100">>}, {-16#10000, <<"39FFFF">>},
+               {-16#10001, <<"3A00010000">>}, {-16#100000000, <<"3AFFFFFFFF">>},
+               {-16#100000001, <<"3B0000000100000000">>}, {-16#10000000000000000, <<"3BFFFFFFFFFFFFFFFF">>},
+               {"Float16 +Inf", positive_infinity, <<"F97C00">>},
+               {"Float16 NaN", nan, <<"F97E00">>},
+               {"Float16 -Inf", negative_infinity, <<"F9FC00">>},
+               {"Float32 +Inf", positive_infinity, <<"FA7F800000">>},
+               {"Float32 NaN", nan, <<"FA7FC00000">>},
+               {"Float32 -Inf", negative_infinity, <<"FAFF800000">>},
+               {"Float64 +Inf", positive_infinity, <<"FB7FF0000000000000">>},
+               {"Float64 NaN", nan, <<"FB7FF8000000000000">>},
+               {"Float64 -Inf", negative_infinity, <<"FBFFF0000000000000">>},
+               {"Binary", <<"">>, <<"40">>},
+               {"Binary", <<1, 2, 3, 4>>, <<"4401020304">>},
+               {"String", <<"">>, <<"60">>},
+               {"String", <<"a">>, <<"6161">>},
+               {"String", <<"IETF">>, <<"6449455446">>},
+               {"String", <<"\"\\">>, <<"62225c">>},
+               {"Date/Time", <<"2013-03-21T20:04:00Z">>, <<"C074323031332D30332D32315432303A30343A30305A">>},
+               {"Timestamp integer", 1363896240, <<"C11A514B67B0">>},
+               {"Timestamp float", 1363896240.5, <<"C1FB41D452D9EC200000">>},
+               {"Tag base16", <<1, 2, 3, 4>>, <<"D74401020304">>},
+               {"Tag CBOR", <<"dIETF">>, <<"D818456449455446">>},
+               {"Tag URI", <<"http://www.example.com">>, <<"D82076687474703A2F2F7777772E6578616D706C652E636F6D">>},
+               {"Tiny Array", {1, 2, 3, 4}, <<"8401020304">>},
+               {"Small Array", list_to_tuple(lists:seq(1, 25)), <<"98190102030405060708090a0b0c0d0e0f101112131415161718181819">>},
+               {"Small Array", erlang:make_tuple(16#FF, 0), <<"98FF", (binary:copy(<<"00">>, 16#FF))/binary>>},
+               {"Short Array", erlang:make_tuple(16#100, 1), <<"990100", (binary:copy(<<"01">>, 16#100))/binary>>},
+               {"Short Array", erlang:make_tuple(16#FFFF, 1), <<"99FFFF", (binary:copy(<<"01">>, 16#FFFF))/binary>>},
+               {"Long Array", erlang:make_tuple(16#10000, 2), <<"9A00010000", (binary:copy(<<"02">>, 16#10000))/binary>>},
+               {"Long Array", erlang:make_tuple(16#FFFFFF, 2), <<"9A00FFFFFF", (binary:copy(<<"02">>, 16#FFFFFF))/binary>>},
+               {"Large Array", lists:duplicate(16#1000000, 2), <<"9A01000000", (binary:copy(<<"02">>, 16#1000000))/binary>>},
+               {"Indefinite Array", [1, 2, 3, 4], <<"9F01020304FF">>},
+               {"Boolean", false, <<"F4">>}, {"Boolean", true, <<"F5">>},
+               {"NULL", null, <<"F6">>}, {"Undefined", undefined, <<"F7">>}]).
 -endif.
